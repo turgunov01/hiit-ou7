a64a9d460e6051d49c4b62e652037734154ece7f
Ваше Имя <you@example.com>
2022-11-30 14:45:26 +0500

    import to github

---
+++ js/script.js
@@ -0,0 +1,73 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  var swiper = new Swiper(".mySwiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        allowSlideNext: false,
+        allowSlidePrev: false,
+      },
+      550: {
+        allowSlideNext: true,
+        allowSlidePrev: true,
+      },
+      600: {
+        slidesPerView: 'auto',
+        spaceBetween: 120
+      },
+      1000: {
+        slidesPerView: 'auto',
+      },
+    },
+
+
+    spaceBetween: 120,
+    centeredSlides: true,
+    slidesPerView: 1,
+
+  });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    centeredSlides: true,
+    // loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 50,
+    speed: 1000,
+    duration: 2000,
+    transition: 500,
+
+    breakpoints: {
+      0: {
+        spaceBetween: 20,
+      },
+
+      400: {
+        spaceBetween: 30,
+      },
+      768: {
+        spaceBetween: 50,
+      }
+    }
+  });
+
+
+  const scene = document.querySelector('.scene');
+  const parallaxInstance = new Parallax(scene, {
+    relativeInput: true,
+    hoverOnly: true
+  })
+});
+
+
+window.onload = function () {
+  if (!window.location.hash) {
+    window.location = window.location + '#loaded';
+    window.location.reload();
+  }
+}
+
